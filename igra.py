c7b675f401f3841b0dc3c70e1cd2c199c6563ff1
student1 <student1@class.local>
2025-09-05 09:19:56 +0900

    добавил игру угадай число

---
+++ igra.py
@@ -0,0 +1,13 @@
+import random 
+def igra():
+    chislo = random.randint(1,10)
+    a = None
+    while a != chislo:
+        a = int(input("угадай число от 1 до 10:"))
+        if a < chislo:
+            print("больше")
+        elif a > chislo:
+            print ("меньше")
+        else:
+            print("правильно")
+igra()
